366672ecd213e23dba9da7201ac3768a08663b7d
Sean M. Brennan <sean_m_brennan@mail.com>
2024-11-25 20:04:55 -0600

    Get library build right

--- local_deploy.sh
+++ local_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t spice-service:latest .
-docker run -p 9988:8000 -it --rm spice-service bash
+docker build -t space-data-service:latest .
+docker run -p 9988:8000 -it --rm space-data-service bash
