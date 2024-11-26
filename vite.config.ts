64635cdc65cc4bc69f675ce61c30e664c8149002
Sean M. Brennan <sean_m_brennan@mail.com>
2024-11-26 09:55:15 -0600

    Disable vite eslint when turbo is active.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
-import { defineConfig } from 'vite'
+import {defineConfig, PluginOption} from 'vite'
 // @ts-expect-error 'types could not be resolved when respecting package.json "exports"'
 import eslint from 'vite-plugin-eslint'
 import dts from 'vite-plugin-dts'
 import {resolve} from "path"
 
+const plugins: PluginOption[] = [
+  dts({
+    rollupTypes: true,
+    outDir: 'dist',
+    tsconfigPath: './tsconfig.app.json',
+  }),
+]
+// vite-plugin-eslint[2] is incompatible with turbo
+if (process.env.TURBO_HASH === undefined) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-call
+  plugins.push(eslint())
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    eslint(),
-    dts({
-      rollupTypes: true,
-      outDir: 'dist',
-      tsconfigPath: './tsconfig.app.json',
-    }),
-  ],
+  plugins: plugins,
+  root: resolve(__dirname),
   base: "/space-data-api/",
   build: {
     lib: {
